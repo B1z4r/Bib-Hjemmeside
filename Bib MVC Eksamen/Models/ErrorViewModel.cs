98b50c0d771d1e857be9e7dc59df40369159e7dd
B1z4r <bastianspil@gmail.com>
2025-03-31 10:20:50 +0200

    Slet

--- Bib MVC Eksamen/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Bib_MVC_Eksamen.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
