ad19693811119d1e2302ee2c53648e57043f8f31
B1z4r <bastianspil@gmail.com>
2025-03-31 09:44:58 +0200

    Bibliotek til eksamen

---
+++ Bib MVC Eksamen/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using Bib_MVC_Eksamen.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Bib_MVC_Eksamen.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
